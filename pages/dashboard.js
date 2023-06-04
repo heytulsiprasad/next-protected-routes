ce9ea676ce3fd22a9913621dcfc6dddf2a6c39c6
Tulsi Prasad <tulsi.prasad50@gmail.com>
2023-06-04 13:04:10 +0530

    feat: add dashboard nav bar and profile menu

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,17 +2,25 @@ import { inter, dancingScript } from "@/utils/fonts";
 import styles from "@/styles/Dashboard.module.scss";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { Button } from "@mantine/core";
+import { useEffect, useRef, useState } from "react";
+import { Avatar, Button } from "@mantine/core";
 import { signOut, getAuth } from "firebase/auth";
 import { notifications } from "@mantine/notifications";
+import Link from "next/link";
+import emailToName from "email-to-name";
+import { useClickOutside } from "@mantine/hooks";
 
 const Dashboard = () => {
+  const [open, setOpen] = useState(true);
   const { setUser, user } = useAuthContext();
   const router = useRouter();
   const auth = getAuth();
+  const profileMenuRef = useClickOutside(() => setOpen(false));
 
-  console.log({ currentUser: user });
+  console.log({
+    user,
+    name: emailToName.process(user.email),
+  });
 
   const logoutHandler = async () => {
     await signOut(auth);
@@ -28,11 +36,27 @@ const Dashboard = () => {
   };
 
   return (
-    <main className={`${dancingScript.className} ${styles.main}`}>
-      <h1>Dashboard</h1>
-      <Button color="red" onClick={logoutHandler}>
-        Logout
-      </Button>
+    <main className={`${inter.className} ${styles.main}`}>
+      <nav className={styles.navbar}>
+        <div className={styles.navItems}>
+          <Link style={{ color: "#000" }} href="/dashboard">
+            Home
+          </Link>
+          <Link href="/bookmarks">Bookmarks</Link>
+        </div>
+        <div className={styles.navProfile}>
+          <Avatar color="cyan" radius="xl" onClick={() => setOpen((c) => !c)}>
+            {emailToName.process(user.email).charAt(0)}
+          </Avatar>
+        </div>
+      </nav>
+      {open && (
+        <div ref={profileMenuRef} className={styles.profileMenu}>
+          <div className={styles.profileMenuItem} onClick={logoutHandler}>
+            <p>Logout</p>
+          </div>
+        </div>
+      )}
     </main>
   );
 };
