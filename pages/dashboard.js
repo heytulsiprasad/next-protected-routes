d62f76f6f760db1934bf3a2f9d549b820498fe71
Tulsi Prasad <tulsi.prasad50@gmail.com>
2023-06-05 12:29:01 +0530

    feat: add get weather feature

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,24 +3,27 @@ import styles from "@/styles/Dashboard.module.scss";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
-import { Avatar, Button } from "@mantine/core";
+import { Avatar, Button, Loader } from "@mantine/core";
 import { signOut, getAuth } from "firebase/auth";
 import { notifications } from "@mantine/notifications";
 import Link from "next/link";
 import emailToName from "email-to-name";
 import { useClickOutside } from "@mantine/hooks";
+import axios from "axios";
+import { AiFillStar, AiOutlineStar, AiFillCloud } from "react-icons/ai";
+import { FiWind } from "react-icons/fi";
+import { WiHumidity } from "react-icons/wi";
 
 const Dashboard = () => {
-  const [open, setOpen] = useState(true);
+  const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [result, setResult] = useState();
+
   const { setUser, user } = useAuthContext();
   const router = useRouter();
   const auth = getAuth();
   const profileMenuRef = useClickOutside(() => setOpen(false));
-
-  console.log({
-    user,
-    name: emailToName.process(user.email),
-  });
 
   const logoutHandler = async () => {
     await signOut(auth);
@@ -33,6 +36,45 @@ const Dashboard = () => {
       message: "You have been logged out successfully",
       color: "green",
     });
+  };
+
+  const getWeather = async (e) => {
+    e.preventDefault();
+
+    const API_KEY = process.env.NEXT_WEATHER_API_KEY;
+    const location = value;
+
+    if (!value) {
+      notifications.show({
+        title: "Error",
+        message: "Please enter a location",
+        color: "red",
+      });
+
+      return;
+    }
+
+    try {
+      setLoading(true);
+
+      const response = await axios.get(
+        `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`
+      );
+
+      console.log(response.data);
+      setResult(response.data);
+      setValue("");
+    } catch (error) {
+      console.log(error.response.data);
+
+      notifications.show({
+        title: "Error",
+        message: error.response.data.error.message,
+        color: "red",
+      });
+    }
+
+    setLoading(false);
   };
 
   return (
@@ -57,6 +99,92 @@ const Dashboard = () => {
           </div>
         </div>
       )}
+
+      {/* Get weather here */}
+      <div className={styles.weatherApp}>
+        {/* Insert location name */}
+        <form className={styles.inputLocation} onSubmit={getWeather}>
+          <h4>Location</h4>
+          <div className={styles.inputBox}>
+            <input
+              type="text"
+              name="location"
+              placeholder="Enter your location"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <Button
+              variant="outline"
+              type="submit"
+              color="pink"
+              onSubmit={getWeather}
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
+
+        {/* Results */}
+        <div className={styles.results}>
+          {loading && <Loader />}
+
+          {result.location && result.current && (
+            <div className={styles.result}>
+              <div className={styles.resultHeader}>
+                <div>
+                  <h1 className={dancingScript.className}>
+                    {result.location.name}, {result.location.region}
+                  </h1>
+                </div>
+                <div className={styles.favIcon}>
+                  <AiOutlineStar
+                    size="24px"
+                    color="#FFDF00"
+                    style={{ marginTop: "5px" }}
+                  />
+                  {/* <AiFillStar
+                    size="24px"
+                    color="#FFDF00"
+                    style={{ marginTop: "5px" }}
+                  /> */}
+                </div>
+              </div>
+              <div className={styles.resultDetails}>
+                <div className={styles.resultDetailsLeft}>
+                  <img
+                    src={result.current.condition.icon}
+                    alt="Weather status"
+                  />
+                </div>
+                <div className={styles.resultDetailsMiddle}>
+                  <h2>{result.current.temp_c}&deg;C</h2>
+                  <p>{result.current.condition.text}</p>
+                </div>
+                <div className={styles.resultDetailsRight}>
+                  <div className={styles.resultDetailsInfo}>
+                    <div>
+                      <AiFillCloud />
+                    </div>
+                    <div>{result.current.cloud}%</div>
+                  </div>
+                  <div className={styles.resultDetailsInfo}>
+                    <div>
+                      <FiWind />
+                    </div>
+                    <div>{result.current.wind_kph}kph</div>
+                  </div>
+                  <div className={styles.resultDetailsInfo}>
+                    <div>
+                      <WiHumidity />
+                    </div>
+                    <div>{result.current.humidity}%</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </main>
   );
 };
